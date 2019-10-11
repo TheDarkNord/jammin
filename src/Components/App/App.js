22dd8396ce28feae819c2d7a808e40371b33de7d
TheDarkNord <30898148+TheDarkNord@users.noreply.github.com>
2019-10-11 16:25:21 +0200

    Updated dependancies

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,12 +5,6 @@ import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchResults } from '../SearchResults/SearchResults';
 import { Playlist } from '../Playlist/Playlist';
 
-// import { Deezer } from'../../util/Deezer';
-
-//Please note, due to lack of support of SPOTIFY in South africa, I tried using a substitue called Deezer. Unfortunately, I was unable to get the dezired result.
-//I have consulted with the team members of Codecademy, and they assured me that if I use the approach recommended by Codecademy and submit, I will not be penalised
-//and the moderators will take into consideration the disadvantage I had to face. I have however included the Deezer approach as point of reference.
-
 import { Spotify } from '../../util/Spotify';
 
 class App extends React.Component {
@@ -25,7 +19,6 @@ class App extends React.Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
-    // this.searchDeezer = this.searchDeezer.bind(this);
     this.searchSpotify = this.searchSpotify.bind(this);
   }
 
@@ -61,13 +54,6 @@ class App extends React.Component {
     this.setState({playlistName: newName});
   }
 
-  // savePlaylist(){
-  //   const trackURI = this.state.playlistTracks.map(trackURI =>{
-  //     return {trackID: trackURI}
-  //   })
-  //   return Deezer.savePlaylist(this.state.playlistName, trackURI);
-  // }
-
   savePlaylist(){
     const trackURI = this.state.playlistTracks.map(trackURI => {
       return {trackID: trackURI}
@@ -75,28 +61,13 @@ class App extends React.Component {
     return Spotify.savePlaylist(this.state.PlaylistName, trackURI)
   }
 
-  // searchDeezer(title) {
-  //   // Deezer.searchDeezer(title);
-  //   var Deezer = require('deezer-node-api');
-  //   var dz = new Deezer();
-  //   var track = title;
-  //   console.log(track);
-  //   let response;
-  //
-  //
-  //   //When called in Deezer.js, the response is a promise, due to asynchronous operation
-  //   return dz.findTracks(track).then(function(result){
-  //   let response = result;
-  //   console.log(response.data);
-  //   console.log(this.state.searchResults);
-  //   response = response.data;
-  //   // this.setState({searchResults: response});
-  //   });
-  // }
-
   searchSpotify(title){
-    const result = Spotify.searchSpotify(title)
-    this.setState({searchResults: result});
+    const result = Spotify.searchSpotify(title);
+    if(result !== undefined){
+      this.setState({searchResults: result});
+    } else {
+      console.log(result);
+    }
   }
 
   render() {
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,49 +1,26 @@
-const apiKey ='';
-const appID = '';
+const apiKey ='968ab6c725a348799c101dd0af193635';
+const appID = '52338a6c67e24458ad65eb04ba54edfb';
+const url = "";
+const RedirectURIL = "http://localhost:3000";
 
 export const Spotify = {
-  searchSpotify(title) {
-    var track = title;
-
-    //Codecademy method for asynchronous requests.
-    const search = async function(){
-      try{
-        let response = await searchSpotify(track).then(function(result){
-          return result
-        });
-        if(response.ok){
-          let jsonResponse = await response.json();
-          console.log(jsonResponse);
-        }
-      } catch(error){
-        console.log(error);
-      }
-    }
-    search(title);
-},
-
-  savePlaylist(playlistName, tracks){
-
-    //Codecademy method for asynchronous POST requests.
-    async function saveList(name, title){
-      const name = playlistName;
-      const title = tracks;
-      const urlWithKey = url + '?key=' + apiKey + '?appID=' + appID;
-      try{
-        let response = await fetch(urlWithKey, {
-          method: 'POST',
-          body: JSON.stringify({playlistName: name}, {trackURI: title}),
-          headers: {"Content-type": "application/json"}
-        });
-        if(response.ok){
-          let jsonResponse = await response.json();
-          return jsonResponse;
-        }
-      } catch(error){
-        console.log(error)
-      }
-    }
-
-    saveList(playlistName, tracks);
-  }
+  // getAccessToken(){
+  //   if(url !== ""){
+  //     return
+  //   } else {
+  //     try{
+  //       let response = await function(){
+  //
+  //       }.then(function(result){
+  //         return result;
+  //       });
+  //       if(response.ok){
+  //         let jsonResponse = await response.json();
+  //         console.log(jsonResponse);
+  //       }
+  //     } catch(error){
+  //       console.log(error);
+  //     }
+  //   }
+  // }
 }
